9f9ca488e5c6887d6da823a8ad4ba9d70e4625c5
Pontus Norup <minecraftpontus@gmail.com>
2024-03-24 18:05:29 +0100

    Added Post and User class and made test posts and users

--- src/server.js
+++ src/server.js
@@ -1,5 +1,23 @@
+import User from "./User.js";
+import Post from "./Post.js";
 import express from "express";
 const app = express();
+
+const users = {
+  user1: new User("Lodjuret2001", "123456789"),
+  user2: new User("Lodjuret2001", "123456789"),
+  user3: new User("Lodjuret2001", "123456789"),
+  user4: new User("Lodjuret2001", "123456789"),
+  user5: new User("Lodjuret2001", "123456789"),
+};
+
+const posts = {
+  post1: new Post("Title 1", "Dummy content"),
+  post2: new Post("Title 2", "Dummy content"),
+  post3: new Post("Title 3", "Dummy content"),
+  post4: new Post("Title 4", "Dummy content"),
+  post5: new Post("Title 5", "Dummy content"),
+};
 
 const PORT = 3000;
 app.listen(PORT, console.log(`Listening on port ${PORT}...`));
